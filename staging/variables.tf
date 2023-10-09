1afa4199eae666c89082f8d9bccbf76cb79e9fe6
vyra <vyra@vyra.vyra.virtualbox.org>
2023-10-09 16:01:25 +0100

    add varied instances

---
+++ staging/variables.tf
@@ -0,0 +1,37 @@
+variable "name" {
+  default = "core01"
+}
+
+variable "machine_type" {
+  type = map
+  default = {
+    dev = "e2-micro",
+    prod = "nl-stanard-1"
+  }
+}
+
+variable "zone" {
+  default = "us-west2-b"
+}
+
+variable "image" {
+  default = "ubuntu-os-cloud/ubuntu-2004-lts"
+}
+
+variable "path" {
+    default = "/home/vyra/Documents/devops/terraform/credentials"
+}
+
+variable "project" {
+  default = "helpful-surge-401108"
+}
+
+variable "region" {
+  default = "us-west2-b"
+}
+
+
+variable "name_count" {
+  default = ["core01", "core02", "core03"]
+  
+}
